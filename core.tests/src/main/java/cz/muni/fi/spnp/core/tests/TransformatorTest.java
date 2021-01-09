36787c2dbeb7ba9b5f4b1f743cdab79753d7a18d
O Marek <xmarek8@fi.muni.cz>
2021-01-09 17:02:28 +0100

    Test functions refactored, simplified

--- core.tests/src/main/java/cz/muni/fi/spnp/core/tests/TransformatorTest.java
+++ core.tests/src/main/java/cz/muni/fi/spnp/core/tests/TransformatorTest.java
@@ -6,7 +6,6 @@ import cz.muni.fi.spnp.core.models.places.StandardPlace;
 import cz.muni.fi.spnp.core.transformators.spnp.SPNPCode;
 import cz.muni.fi.spnp.core.transformators.spnp.SPNPOptions;
 import cz.muni.fi.spnp.core.transformators.spnp.SPNPTransformator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.regex.Matcher;
 
@@ -48,6 +47,34 @@ public abstract class TransformatorTest {
         return placeWrapper;
     }
     
+    private boolean checkInputParameter(String line, HashSet<String> tmpInputParameters){
+        boolean success = true;
+        Matcher matcher = TestPatterns.inputParameterExtractRegex.matcher(line);
+        if (matcher.find())
+        {
+            var inputParameter = matcher.group(1);
+            if(!tmpInputParameters.remove(inputParameter)){
+                System.out.println("Input parameter was not defined: " + inputParameter);
+                success = false;
+            }
+        }
+        return success;
+    }
+
+    private boolean checkOption(String line, HashSet<String> tmpOptionKeys){
+        boolean success = true;
+        Matcher matcher = TestPatterns.optionKeyExtractRegex.matcher(line);
+        if (matcher.find())
+        {
+            var optionKey = matcher.group(1);
+            if(!tmpOptionKeys.remove(optionKey)){
+                System.out.println("Option with the following key was not defined: " + optionKey);
+                success = false;
+            }
+        }
+        return success;
+    }
+    
     private boolean runOptionsTest(String output){
         boolean success = true;
         
@@ -70,26 +97,10 @@ public abstract class TransformatorTest {
         for(var line : lines){
             if(optionsStarted){
                 if(TestPatterns.inputParameterRegex.matcher(line).matches()){
-                    Matcher matcher = TestPatterns.inputParameterExtractRegex.matcher(line);
-                    if (matcher.find())
-                    {
-                        var inputParameter = matcher.group(1);
-                        if(!tmpInputParameters.remove(inputParameter)){
-                            System.out.println("Input parameter was not defined: " + inputParameter);
-                            success = false;
-                        }
-                    }
+                    success = checkInputParameter(line, tmpInputParameters);
                 }
                 else if(TestPatterns.optionsRegex.matcher(line).matches()){
-                    Matcher matcher = TestPatterns.optionKeyExtractRegex.matcher(line);
-                    if (matcher.find())
-                    {
-                        var optionKey = matcher.group(1);
-                        if(!tmpOptionKeys.remove(optionKey)){
-                            System.out.println("Option with the following key was not defined: " + optionKey);
-                            success = false;
-                        }
-                    }
+                    success = checkOption(line, tmpOptionKeys);
                 }
                 else if(TestPatterns.functionClosedRegex.matcher(line).matches()){
                     optionsClosed = true;
@@ -131,6 +142,140 @@ public abstract class TransformatorTest {
         return success;
     }
     
+    private boolean checkStandardPlaceDeclaration(String line, HashSet<StandardPlaceTestWrapper> tmpStandardPlaces){
+        boolean success = true;
+        Matcher matcher = TestPatterns.standardPlaceDeclarationExtractRegex.matcher(line);
+        if (matcher.find())
+        {
+            var placeDeclarationName = matcher.group(1);
+            boolean found = false;
+            for(var place : tmpStandardPlaces){
+                if(place.getName().equals(placeDeclarationName)){
+                    place.setDeclared(true);
+                    found = true;
+                }
+            }
+            if(!found){
+                System.out.println("Standard place was declared but should not be: " + placeDeclarationName);
+                success = false;
+            }
+        }
+        return success;
+    }
+    
+    private boolean checkStandardPlaceInit(String line, HashSet<StandardPlaceTestWrapper> tmpStandardPlaces){
+        boolean success = true;
+        Matcher matcher = TestPatterns.standardPlaceInitNameExtractRegex.matcher(line);
+        if (matcher.find())
+        {
+            var placeInitName = matcher.group(1);
+            boolean found = false;
+            for(var place : tmpStandardPlaces){
+                if(place.getName().equals(placeInitName)){
+                    place.setDefaultValueInited(true);
+                    found = true;
+                }
+            }
+            if(!found){
+                System.out.println("Standard place default value was initialized but should not be: " + placeInitName);
+                success = false;
+            }
+        }
+        return success;
+    }
+    
+    private boolean checkFluidPlaceDeclaration(String line, HashSet<FluidPlaceTestWrapper> tmpFluidPlaces){
+        boolean success = true;
+        Matcher matcher = TestPatterns.fluidPlaceDeclarationExtractRegex.matcher(line);
+        if (matcher.find())
+        {
+            var placeDeclarationName = matcher.group(1);
+            boolean found = false;
+            for(var place : tmpFluidPlaces){
+                if(place.getName().equals(placeDeclarationName)){
+                    place.setDeclared(true);
+                    found = true;
+                }
+            }
+            if(!found){
+                System.out.println("Fluid place was declared but should not be: " + placeDeclarationName);
+                success = false;
+            }
+        }
+        return success;
+    }
+    
+    private boolean checkFluidPlaceInit(String line, HashSet<FluidPlaceTestWrapper> tmpFluidPlaces){
+        boolean success = true;
+        Matcher matcher = TestPatterns.fluidPlaceInitNameExtractRegex.matcher(line);
+        if (matcher.find())
+        {
+            var placeInitName = matcher.group(1);
+            boolean found = false;
+            for(var place : tmpFluidPlaces){
+                if(place.getName().equals(placeInitName)){
+                    place.setDefaultValueInited(true);
+                    found = true;
+                }
+            }
+            if(!found){
+                System.out.println("Fluid place default value was initialized but should not be: " + placeInitName);
+                success = false;
+            }
+        }
+        return success;
+    }
+    
+    private boolean checkFluidPlaceBoundValue(String line, HashSet<FluidPlaceTestWrapper> tmpFluidPlaces){
+        boolean success = true;
+        Matcher matcher = TestPatterns.fluidPlaceBoundNameExtractRegex.matcher(line);
+        if (matcher.find())
+        {
+            var placeBoundName = matcher.group(1);
+            boolean found = false;
+            for(var place : tmpFluidPlaces){
+                if(place.getName().equals(placeBoundName)){
+                    place.setBoundValueInited(true);
+                    found = true;
+                }
+            }
+            if(!found){
+                System.out.println("Fluid place bound value was initialized but should not be: " + placeBoundName);
+                success = false;
+            }
+        }
+        return success;
+    }
+    
+    private boolean checkFluidPlaceBreakValue(String line, HashSet<FluidPlaceTestWrapper> tmpFluidPlaces){
+        boolean success = true;
+        Matcher nameMatcher = TestPatterns.fluidPlaceBreakNameExtractRegex.matcher(line);
+        Matcher valueMatcher = TestPatterns.fluidPlaceBreakValueExtractRegex.matcher(line);
+        if (nameMatcher.find())
+        {
+            var placeBreakName = nameMatcher.group(1);
+            if(valueMatcher.find()){
+                var placeBreakValue = Double.parseDouble(valueMatcher.group(1));
+
+                boolean found = false;
+                for(var place : tmpFluidPlaces){
+                    if(place.getName().equals(placeBreakName)){
+                        var breakValues = place.getBreakValuesInited();
+                        if(breakValues.get(placeBreakValue) != null){
+                            place.setBreakValueInited(placeBreakValue, true);
+                            found = true;
+                        }
+                    }
+                }
+                if(!found){
+                    System.out.println("Fluid place break value was initialized but should not be: " + placeBreakName + " break value: " + placeBreakValue);
+                    success = false;
+                }
+            }
+        }
+        return success;
+    }
+    
     private boolean runNetTest(String output){
         boolean success = true;
         System.out.println("Net test:");
@@ -153,120 +298,22 @@ public abstract class TransformatorTest {
         for(var line : lines){
             if(netStarted){
                 if(TestPatterns.standardPlaceDeclarationRegex.matcher(line).matches()){
-                    Matcher matcher = TestPatterns.standardPlaceDeclarationExtractRegex.matcher(line);
-                    if (matcher.find())
-                    {
-                        var placeDeclarationName = matcher.group(1);
-                        boolean found = false;
-                        for(var place : tmpStandardPlaces){
-                            if(place.getName().equals(placeDeclarationName)){
-                                place.setDeclared(true);
-                                found = true;
-                            }
-                        }
-                        if(!found){
-                            System.out.println("Standard place was declared but should not be: " + placeDeclarationName);
-                            success = false;
-                        }
-                    }
+                    success = checkStandardPlaceDeclaration(line, tmpStandardPlaces);
                 }
                 else if(TestPatterns.standardPlaceInitRegex.matcher(line).matches()){
-                    Matcher matcher = TestPatterns.standardPlaceInitNameExtractRegex.matcher(line);
-                    if (matcher.find())
-                    {
-                        var placeInitName = matcher.group(1);
-                        boolean found = false;
-                        for(var place : tmpStandardPlaces){
-                            if(place.getName().equals(placeInitName)){
-                                place.setDefaultValueInited(true);
-                                found = true;
-                            }
-                        }
-                        if(!found){
-                            System.out.println("Standard place default value was initialized but should not be: " + placeInitName);
-                            success = false;
-                        }
-                    }
+                    success = checkStandardPlaceInit(line, tmpStandardPlaces);
                 }
                 else if(TestPatterns.fluidPlaceDeclarationRegex.matcher(line).matches()){
-                    Matcher matcher = TestPatterns.fluidPlaceDeclarationExtractRegex.matcher(line);
-                    if (matcher.find())
-                    {
-                        var placeDeclarationName = matcher.group(1);
-                        boolean found = false;
-                        for(var place : tmpFluidPlaces){
-                            if(place.getName().equals(placeDeclarationName)){
-                                place.setDeclared(true);
-                                found = true;
-                            }
-                        }
-                        if(!found){
-                            System.out.println("Fluid place was declared but should not be: " + placeDeclarationName);
-                            success = false;
-                        }
-                    }
+                    success = checkFluidPlaceDeclaration(line, tmpFluidPlaces);
                 }
                 else if(TestPatterns.fluidPlaceInitRegex.matcher(line).matches()){
-                    Matcher matcher = TestPatterns.fluidPlaceInitNameExtractRegex.matcher(line);
-                    if (matcher.find())
-                    {
-                        var placeInitName = matcher.group(1);
-                        boolean found = false;
-                        for(var place : tmpFluidPlaces){
-                            if(place.getName().equals(placeInitName)){
-                                place.setDefaultValueInited(true);
-                                found = true;
-                            }
-                        }
-                        if(!found){
-                            System.out.println("Fluid place default value was initialized but should not be: " + placeInitName);
-                            success = false;
-                        }
-                    }
+                    success = checkFluidPlaceInit(line, tmpFluidPlaces);
                 }
                 else if(TestPatterns.fluidPlaceBoundRegex.matcher(line).matches()){
-                    Matcher matcher = TestPatterns.fluidPlaceBoundNameExtractRegex.matcher(line);
-                    if (matcher.find())
-                    {
-                        var placeBoundName = matcher.group(1);
-                        boolean found = false;
-                        for(var place : tmpFluidPlaces){
-                            if(place.getName().equals(placeBoundName)){
-                                place.setBoundValueInited(true);
-                                found = true;
-                            }
-                        }
-                        if(!found){
-                            System.out.println("Fluid place bound value was initialized but should not be: " + placeBoundName);
-                            success = false;
-                        }
-                    }
+                    success = checkFluidPlaceBoundValue(line, tmpFluidPlaces);
                 }
                 else if(TestPatterns.fluidPlaceBreakRegex.matcher(line).matches()){
-                    Matcher nameMatcher = TestPatterns.fluidPlaceBreakNameExtractRegex.matcher(line);
-                    Matcher valueMatcher = TestPatterns.fluidPlaceBreakValueExtractRegex.matcher(line);
-                    if (nameMatcher.find())
-                    {
-                        var placeBreakName = nameMatcher.group(1);
-                        if(valueMatcher.find()){
-                            var placeBreakValue = Double.parseDouble(valueMatcher.group(1));
-                            
-                            boolean found = false;
-                            for(var place : tmpFluidPlaces){
-                                if(place.getName().equals(placeBreakName)){
-                                    var breakValues = place.getBreakValuesInited();
-                                    if(breakValues.get(placeBreakValue) != null){
-                                        place.setBreakValueInited(placeBreakValue, true);
-                                        found = true;
-                                    }
-                                }
-                            }
-                            if(!found){
-                                System.out.println("Fluid place break value was initialized but should not be: " + placeBreakName + " break value: " + placeBreakValue);
-                                success = false;
-                            }
-                        }
-                    }
+                    success = checkFluidPlaceBreakValue(line, tmpFluidPlaces);
                 }
                 else if(TestPatterns.functionClosedRegex.matcher(line).matches()){
                     netClosed = true;
