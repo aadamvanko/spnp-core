fb664704e29797822b14b2a8c5929d3c8ce95090
Adam Vanko <aadamvanko@gmail.com>
2021-02-13 14:05:59 +0100

    Add test class for PetriNet.

---
+++ models/src/test/java/cz/muni/fi/spnp/core/models/PetriNetTest.java
@@ -0,0 +1,19 @@
+package cz.muni.fi.spnp.core.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PetriNetTest {
+
+    private PetriNet petriNet;
+
+    @Before
+    public void createPetriNet() {
+        petriNet = new PetriNet();
+    }
+
+    @Test
+    public void test() {
+    }
+
+}
