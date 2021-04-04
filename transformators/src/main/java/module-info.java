20c1bf9582369222b8e2474f6cb910b37c7ada88
10ondr <10ondr@seznam.cz>
2021-04-05 00:35:38 +0200

    Added exports for Transformators

--- transformators/src/main/java/module-info.java
+++ transformators/src/main/java/module-info.java
@@ -1,3 +1,9 @@
 module spnp.core.transformators {
     requires spnp.core.models;
+
+    exports cz.muni.fi.spnp.core.transformators.spnp;
+    exports cz.muni.fi.spnp.core.transformators.spnp.code;
+    exports cz.muni.fi.spnp.core.transformators.spnp.options;
+    exports cz.muni.fi.spnp.core.transformators.spnp.parameters;
+    exports cz.muni.fi.spnp.core.transformators.spnp.distributions;
 }
