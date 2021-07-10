b032f3ecc559b3a94e26409e25207987b37afcf7
Adam Vanko <aadamvanko@gmail.com>
2021-07-10 11:25:15 +0200

    Add constructor without parameter tokens.

--- transformators/src/main/java/cz/muni/fi/spnp/core/transformators/spnp/elements/SPNPStandardPlace.java
+++ transformators/src/main/java/cz/muni/fi/spnp/core/transformators/spnp/elements/SPNPStandardPlace.java
@@ -6,6 +6,10 @@ public class SPNPStandardPlace extends StandardPlace {
 
     private final String numberOfTokensExpression;
 
+    public SPNPStandardPlace(int id, String name) {
+        this(id, name, "0");
+    }
+
     public SPNPStandardPlace(int id, String name, String numberOfTokensExpression) {
         super(id, name);
 
